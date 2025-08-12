c7629912a5d2a6121c06d95d8751b9808d9b371e
neo <neo.dowithless@gmail.com>
2025-08-12 17:20:57 +0800

    chore: update sitemap config

--- docs/next-sitemap.config.js
+++ docs/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://jan.ai/',
+  siteUrl: 'https://jan-zh.readme-i18n.com/',
   generateRobotsTxt: true,
   changefreq: 'daily',
   priority: 1.0,
